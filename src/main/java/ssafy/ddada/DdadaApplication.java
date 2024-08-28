c85177cdb60c71085145d4886bf62f8d00f306ac
정한수 <jounghs3933@gmail.com>
2024-08-28 15:10:26 +0900

    프로젝트 생성

---
+++ src/main/java/ssafy/ddada/DdadaApplication.java
@@ -0,0 +1,13 @@
+package ssafy.ddada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdadaApplication.class, args);
+    }
+
+}
