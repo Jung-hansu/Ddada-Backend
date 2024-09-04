3a504b53891d7599873d51cec7fbf44d58003042
정한수 <jounghs3933@gmail.com>
2024-09-04 17:14:11 +0900

    feat : Redis Cluster 연결 및 템플릿 설정

--- src/main/java/ssafy/ddada/config/PropertiesConfig.java
+++ src/main/java/ssafy/ddada/config/PropertiesConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import ssafy.ddada.common.properties.CorsProperties;
 import ssafy.ddada.common.properties.JwtProperties;
 import ssafy.ddada.common.properties.KakaoLoginProperties;
+import ssafy.ddada.common.properties.RedisProperties;
 
 @Configuration
 @EnableConfigurationProperties({
         CorsProperties.class,
         KakaoLoginProperties.class,
         JwtProperties.class,
+        RedisProperties.class
 })
 public class PropertiesConfig {
 }
