e447ee7dadb8bca87e2db5dfc497b6ab74a80905
정한수 <jounghs3933@gmail.com>
2024-09-06 09:37:55 +0900

    refactor : http status에 사전 정의 상수 활용

--- src/main/java/ssafy/ddada/common/exception/errorcode/MatchErrorCode.java
+++ src/main/java/ssafy/ddada/common/exception/errorcode/MatchErrorCode.java
@@ -3,11 +3,13 @@ package ssafy.ddada.common.exception.errorcode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static ssafy.ddada.api.StatusCode.*;
+
 @Getter
 @AllArgsConstructor
 public enum MatchErrorCode implements BaseErrorCode{
 
-    MATCH_NOT_FOUND(404, "MATCH_404_1", "존재하지 않는 경기입니다.");
+    MATCH_NOT_FOUND(NOT_FOUND, "MATCH_404_1", "존재하지 않는 경기입니다.");
 
     private final Integer httpStatus;
     private final String code;
