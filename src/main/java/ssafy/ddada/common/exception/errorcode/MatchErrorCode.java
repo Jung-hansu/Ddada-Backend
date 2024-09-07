9115315d20bd9485afa9d6fe221c4e37d5225f86
정한수 <jounghs3933@gmail.com>
2024-09-07 16:50:27 +0900

    feat : Exception 추가

--- src/main/java/ssafy/ddada/common/exception/errorcode/MatchErrorCode.java
+++ src/main/java/ssafy/ddada/common/exception/errorcode/MatchErrorCode.java
@@ -9,7 +9,8 @@ import static ssafy.ddada.api.StatusCode.*;
 @AllArgsConstructor
 public enum MatchErrorCode implements BaseErrorCode{
 
-    MATCH_NOT_FOUND(NOT_FOUND, "MATCH_404_1", "존재하지 않는 경기입니다.");
+    MATCH_NOT_FOUND(NOT_FOUND, "MATCH_404_1", "존재하지 않는 경기입니다."),
+    TEAM_NOT_FOUND(NOT_FOUND, "TEAM_404_1", "존재하지 않는 팀입니다.");
 
     private final Integer httpStatus;
     private final String code;
--- src/main/java/ssafy/ddada/common/handler/MatchExceptionHandler.java
+++ src/main/java/ssafy/ddada/common/handler/MatchExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ssafy.ddada.api.CommonResponse;
 import ssafy.ddada.common.exception.MatchNotFoundException;
+import ssafy.ddada.common.exception.TeamNotFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,8 +16,16 @@ public class MatchExceptionHandler {
     @ExceptionHandler(MatchNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public CommonResponse<?> handleMatchNotFoundException(MatchNotFoundException e) {
-        log.error("matchNotFoundException", e);
+        log.error("MatchNotFoundException occurs", e);
         return CommonResponse.notFound(e.getErrorCode());
     }
+
+    @ExceptionHandler(TeamNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public CommonResponse<?> handleTeamNotFoundException(TeamNotFoundException e) {
+        log.error("TeamNotFoundException occurs", e);
+        return CommonResponse.notFound(e.getErrorCode());
+    }
+
 
 }
