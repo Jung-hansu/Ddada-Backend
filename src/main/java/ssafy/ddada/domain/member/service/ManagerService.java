8cd490dbf8f1e70f2684d136ee2f09e693d822c1
정한수 <jounghs3933@gmail.com>
2024-09-09 12:11:06 +0900

    remove : 불필요한 파일 통합 및 삭제

--- src/main/java/ssafy/ddada/domain/member/service/ManagerService.java
+++
@@ -1,9 +0,0 @@
-package ssafy.ddada.domain.member.service;
-
-import ssafy.ddada.domain.member.entity.SearchedManager;
-
-public interface ManagerService {
-
-    SearchedManager getManagerById(Long managerId);
-
-}
