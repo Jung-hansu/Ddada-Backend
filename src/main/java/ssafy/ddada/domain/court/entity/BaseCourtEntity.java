b3597808003b05f435276f59eb6249918709b8ec
정한수 <jounghs3933@gmail.com>
2024-09-10 17:29:08 +0900

    feat : 시설 엔티티에 수정일시 추가

--- src/main/java/ssafy/ddada/domain/court/entity/BaseCourtEntity.java
+++ src/main/java/ssafy/ddada/domain/court/entity/BaseCourtEntity.java
@@ -3,6 +3,7 @@ package ssafy.ddada.domain.court.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -17,4 +18,7 @@ public class BaseCourtEntity {
     @CreatedDate
     @Column(updatable = false)
     LocalDateTime createdAt;
+
+    @LastModifiedDate
+    LocalDateTime updatedAt;
 }
