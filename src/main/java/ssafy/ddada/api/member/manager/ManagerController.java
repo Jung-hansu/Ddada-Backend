b4f20f852c7fb8b5a74abe7baaf8e23bab8c3824
CSchoice <tkdlqjf@gmail.com>
2024-09-13 14:00:15 +0900

    feat : 테스트용 매니저 생성 기능 추가

--- src/main/java/ssafy/ddada/api/member/manager/ManagerController.java
+++ src/main/java/ssafy/ddada/api/member/manager/ManagerController.java
@@ -5,12 +5,17 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ssafy.ddada.api.CommonResponse;
+import ssafy.ddada.api.member.manager.request.ManagerSignupRequest;
 import ssafy.ddada.api.member.manager.response.ManagerDetailResponse;
 import ssafy.ddada.api.match.request.MatchResultRequest;
 import ssafy.ddada.api.match.response.MatchSimpleResponse;
+import ssafy.ddada.api.member.manager.response.ManagerSignupResponse;
+import ssafy.ddada.api.member.player.response.PlayerSignupResponse;
 import ssafy.ddada.common.util.SecurityUtil;
+import ssafy.ddada.domain.member.manager.entity.Manager;
 import ssafy.ddada.domain.member.manager.service.ManagerService;
 import ssafy.ddada.domain.match.service.MatchService;
 
@@ -26,7 +31,7 @@ public class ManagerController {
 
     @Operation(summary = "매니저 세부 조회", description = "매니저 세부 정보를 조회하는 api입니다.")
     @GetMapping
-    public CommonResponse<ManagerDetailResponse> getManager(){
+    public CommonResponse<ManagerDetailResponse> getManager() {
         Long managerId = SecurityUtil.getLoginMemberId();
         log.info("매니저 세부 조회 >>>> 매니저 ID: {}", managerId);
 
@@ -36,7 +41,7 @@ public class ManagerController {
 
     @Operation(summary = "매니저 할당 경기 리스트 조회", description = "매니저에게 할당된 경기 리스트를 조회하는 api입니다.")
     @GetMapping("/matches")
-    public CommonResponse<Page<MatchSimpleResponse>> getAllocatedMatches(@RequestParam("page") Integer page, @RequestParam("size") Integer size){
+    public CommonResponse<Page<MatchSimpleResponse>> getAllocatedMatches(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
         Long managerId = SecurityUtil.getLoginMemberId();
         log.info("매니저 경기 리스트 조회 >>>> 매니저 ID: {}", managerId);
 
@@ -46,7 +51,7 @@ public class ManagerController {
 
     @Operation(summary = "할당된 경기 저장", description = "경기 종료 후 경기 데이터를 저장하는 api입니다.")
     @PutMapping("/matches/{match_id}")
-    public CommonResponse<?> saveMatch(@PathVariable("match_id") Long matchId, @RequestBody MatchResultRequest request){
+    public CommonResponse<?> saveMatch(@PathVariable("match_id") Long matchId, @RequestBody MatchResultRequest request) {
 //        TODO: 이건 매니저 검사용으로만 쓰기. managerId는 비즈니스 로직상 안씀
         Long managerId = SecurityUtil.getLoginMemberId();
         log.info("할당된 경기 저장 >>>> 매니저 ID: {}, 경기 ID: {}", managerId, matchId);
@@ -56,4 +61,15 @@ public class ManagerController {
         return CommonResponse.ok("저장되었습니다.", null);
     }
 
+    @Operation(summary = "매니저 회원가입", description = "매니저 회원가입 api입니다.")
+    @PostMapping(value = "/signup", consumes = { "multipart/form-data" })
+    public CommonResponse<?> signup(
+            @ModelAttribute @Validated ManagerSignupRequest request
+    ) {
+        ManagerSignupResponse response = managerService.signupManager(request.toCommand());
+
+        return CommonResponse.ok(response);
+    }
 }
+
+
