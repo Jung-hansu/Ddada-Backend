509450cae4580371589e2f228e1f62882aaeb181
CSchoice <tkdlqjf@gmail.com>
2024-09-13 10:30:35 +0900

    fix : s3 presignedurl 로직 수정

--- src/main/java/ssafy/ddada/common/util/S3Util.java
+++ src/main/java/ssafy/ddada/common/util/S3Util.java
@@ -8,12 +8,17 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import ssafy.ddada.common.exception.NotAllowedExtensionException;
 import ssafy.ddada.common.exception.ProfileNotFoundInS3Exception;
 import ssafy.ddada.common.properties.S3Properties;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -24,13 +29,15 @@ public class S3Util {
 
     private final AmazonS3 amazonS3Client;
     private final S3Properties s3Properties;
+    private final S3Presigner s3Presigner;
 
-    public S3Util(AmazonS3 amazonS3Client, S3Properties s3Properties) {
+    public S3Util(AmazonS3 amazonS3Client, S3Properties s3Properties, S3Presigner s3Presigner) {
         this.amazonS3Client = amazonS3Client;
         this.s3Properties = s3Properties;
+        this.s3Presigner = s3Presigner; // 올바르게 주입
     }
 
-    public String uploadImageToS3(MultipartFile image, Long memberId, String Location) {
+    public String uploadImageToS3(MultipartFile image, Long memberId, String location) {
         if (image == null || image.isEmpty()) {
             return null;
         }
@@ -47,7 +54,7 @@ public class S3Util {
             throw new NotAllowedExtensionException();
         }
 
-        String fileName = Location + memberId + extension;
+        String fileName = location + memberId + extension;
 
         try {
             amazonS3Client.putObject(new PutObjectRequest(s3Properties.s3().bucket(), fileName, image.getInputStream(), null));
@@ -59,20 +66,28 @@ public class S3Util {
 
     public String getPresignedUrlFromS3(String imagePath) {
         try {
-            GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(s3Properties.s3().bucket(), imagePath)
-                    .withMethod(HttpMethod.GET)
-                    .withExpiration(getExpirationTime());
+            String objectKey = imagePath.replace("https://ddada-image.s3.ap-northeast-2.amazonaws.com/", "");
 
-            URL presignedUrl = amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
-            log.info(imagePath + " 이미지에 대한 presigned URL 생성 성공");
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(s3Properties.s3().bucket())
+                    .key(objectKey)
+                    .build();
+
+            GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
+                    .getObjectRequest(getObjectRequest)
+                    .signatureDuration(Duration.ofMinutes(10))
+                    .build();
+
+            PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(getObjectPresignRequest);
+            URL presignedUrl = presignedRequest.url();
+
+            log.info(objectKey + " 이미지에 대한 presigned URL 생성 성공");
+            log.info("presigned URL: " + presignedUrl.toString());
 
             return presignedUrl.toString();
-        } catch (AmazonServiceException e) {
-            if (e.getStatusCode() == 404) {
-                return null;
-            } else {
-                throw new ProfileNotFoundInS3Exception();
-            }
+        } catch (Exception e) {
+            log.error("Presigned URL 생성 중 오류 발생: " + e.getMessage(), e);
+            throw new RuntimeException("Presigned URL 생성 실패", e);
         }
     }
 
