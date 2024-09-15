d3c2363da86084df78bd1e7cda9721d9e5284857
정한수 <jounghs3933@gmail.com>
2024-09-16 04:46:30 +0900

    feat : 매니저 할당 경기 검색 요청 및 커맨드 DTO 추가

---
+++ src/main/java/ssafy/ddada/api/member/manager/request/ManagerSearchMatchRequest.java
@@ -0,0 +1,28 @@
+package ssafy.ddada.api.member.manager.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.data.domain.PageRequest;
+import ssafy.ddada.domain.match.entity.MatchStatus;
+import ssafy.ddada.domain.member.manager.command.ManagerSearchMatchCommand;
+
+import static ssafy.ddada.common.util.ParameterUtil.nullToFalse;
+
+@Schema(description = "매니저 할당 경기 검색 결과")
+public record ManagerSearchMatchRequest(
+        Long managerId,
+        String keyword,
+        Boolean todayOnly,
+        String statuses,
+        int page,
+        int size
+) {
+    public ManagerSearchMatchCommand toCommand(){
+        return new ManagerSearchMatchCommand(
+                managerId,
+                keyword,
+                nullToFalse(todayOnly),
+                MatchStatus.toMatchStatusSet(statuses),
+                PageRequest.of(page, size)
+        );
+    }
+}
