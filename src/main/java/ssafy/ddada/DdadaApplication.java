b24a1c02b549a7b4493b1c457d3297b04b2509f7
정한수 <jounghs3933@gmail.com>
2024-09-25 09:14:02 +0900

    chore : createdAt, updatedAt이 적용되도록 어노테이션 추가

--- src/main/java/ssafy/ddada/DdadaApplication.java
+++ src/main/java/ssafy/ddada/DdadaApplication.java
@@ -3,9 +3,11 @@ package ssafy.ddada;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+@EnableJpaAuditing
 @EnableFeignClients
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
