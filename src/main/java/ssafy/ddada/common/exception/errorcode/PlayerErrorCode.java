89db3ff99d8a27b938791b9d2875044fdae1c265
CSchoice <tkdlqjf@gmail.com>
2024-09-25 11:27:11 +0900

    fix : 비밀번호 수정 에러 핸들러 추가

--- src/main/java/ssafy/ddada/common/exception/errorcode/PlayerErrorCode.java
+++ src/main/java/ssafy/ddada/common/exception/errorcode/PlayerErrorCode.java
@@ -12,6 +12,7 @@ public enum PlayerErrorCode implements BaseErrorCode {
     NICKNAME_DUPLICATE(BAD_REQUEST, "PLAYER_400_2", "이미 존재하는 닉네임입니다."),
     PLAYER_DUPLICATE(BAD_REQUEST, "PLAYER_400_3", "이미 존재하는 회원입니다."),
     MESSAGE_SENDING(BAD_REQUEST, "PLAYER_400_4", "메시지 전송에 실패하였습니다."),
+    PASSWORD_NOT_MATCH(NOT_FOUND, "PLAYER_400_5", "비밀번호가 일치하지 않습니다."),
 
     INVALID_LOGIN_INFO(UNAUTHORIZED, "PLAYER_401_1", "로그인 유저가 존재하지 않습니다."),
 
@@ -20,10 +21,9 @@ public enum PlayerErrorCode implements BaseErrorCode {
     PROFILE_NOT_FOUND(NOT_FOUND, "PLAYER_404_3", "프로필 이미지를 찾을 수 없습니다."),
     VERIFICATION_FAILURE(NOT_FOUND, "PLAYER_404_4", "인증에 실패하였습니다."),
     EMAIL_NOT_FOUND(NOT_FOUND, "PLAYER_404_5", "존재하지 않는 이메일입니다."),
-    PASSWORD_NOT_MATCH(NOT_FOUND, "PLAYER_404_6", "비밀번호가 일치하지 않습니다."),
-    PHONE_NUMBER_NOT_FOUND(NOT_FOUND, "PLAYER_404_7", "존재하지 않는 전화번호입니다."),
-    KAKAO_MAIL_PLAYER_NOT_FOUND(NOT_FOUND, "PLAYER_404_8", "카카오 메일에 해당하는 회원을 찾을 수 없습니다."),
-    LOGIN_TYPE_NOT_SUPPORTED(NOT_FOUND, "PLAYER_404_9", "지원하지 않는 로그인 타입입니다."),
+    PHONE_NUMBER_NOT_FOUND(NOT_FOUND, "PLAYER_404_6", "존재하지 않는 전화번호입니다."),
+    KAKAO_MAIL_PLAYER_NOT_FOUND(NOT_FOUND, "PLAYER_404_&", "카카오 메일에 해당하는 회원을 찾을 수 없습니다."),
+    LOGIN_TYPE_NOT_SUPPORTED(NOT_FOUND, "PLAYER_404_8", "지원하지 않는 로그인 타입입니다."),
 
     PLAYER_ALREADY_DELETED(CONFLICT, "PLAYER_409_1", "이미 삭제된 회원입니다."),
 
