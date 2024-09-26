beab70bb5aa25c5eb69ab6f60ee21e34cb1ebaac
정한수 <jounghs3933@gmail.com>
2024-09-26 09:55:56 +0900

    fix : 특정 멤버에 nullable 제약조건 추가

--- src/main/java/ssafy/ddada/domain/racket/entity/Racket.java
+++ src/main/java/ssafy/ddada/domain/racket/entity/Racket.java
@@ -17,22 +17,28 @@ public class Racket {
     @Column(name = "racket_id")
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String manufacturer;
 
+    @Column(nullable = false)
     private Integer price;
 
     private String balance;
 
+    @Column(nullable = false)
     private String weight;
 
     private String shaft;
 
+    @Column(nullable = false)
     private String material;
 
     private String color;
 
+    @Column(nullable = false)
     private String image;
 
 }
