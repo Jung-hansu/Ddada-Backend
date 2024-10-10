f16d41830d5195bfd96ea8e8bf97bf156a1fbbed
CSchoice <tkdlqjf@gmail.com>
2024-10-10 21:04:06 +0900

    hotfix : 매치 조회 시 선수 성별 조회 로직 수정

--- src/main/java/ssafy/ddada/domain/match/entity/Match.java
+++ src/main/java/ssafy/ddada/domain/match/entity/Match.java
@@ -76,28 +76,63 @@ public class Match extends BaseMatchEntity {
         Player player1 = team.getPlayer1();
         Player player2 = team.getPlayer2();
 
-        // First, add male players
+        String malePlayer1 = null;
+        String malePlayer2 = null;
+        String femalePlayer1 = null;
+        String femalePlayer2 = null;
+
+        // 남자 플레이어 찾기
         if (player1 != null && player1.getGender() == Gender.MALE) {
-            teamGender.add(player1.getGender().name());
+            malePlayer1 = player1.getGender().name();
         }
         if (player2 != null && player2.getGender() == Gender.MALE) {
-            teamGender.add(player2.getGender().name());
+            if (malePlayer1 == null) {
+                malePlayer1 = player2.getGender().name();
+            } else {
+                malePlayer2 = player2.getGender().name();
+            }
         }
 
-        // Then, add female players
+        // 여자 플레이어 찾기
         if (player1 != null && player1.getGender() == Gender.FEMALE) {
-            teamGender.add(player1.getGender().name());
+            femalePlayer1 = player1.getGender().name();
         }
         if (player2 != null && player2.getGender() == Gender.FEMALE) {
-            teamGender.add(player2.getGender().name());
+            if (femalePlayer1 == null) {
+                femalePlayer1 = player2.getGender().name();
+            } else {
+                femalePlayer2 = player2.getGender().name();
+            }
         }
 
-        // If a player is not reserved, add "notReserved"
-        if (player1 == null) {
-            teamGender.add("notReserved");
-        }
-        if (player2 == null) {
-            teamGender.add("notReserved");
+        // 혼성팀일 경우 남자는 0번 인덱스, 여자는 1번 인덱스에 추가
+        if (malePlayer1 != null && femalePlayer1 != null) {
+            teamGender.add(0, malePlayer1); // 남자를 0번 인덱스에 추가
+            teamGender.add(1, femalePlayer1); // 여자를 1번 인덱스에 추가
+        } else if (malePlayer1 != null && malePlayer2 != null) {
+            // 남자 두 명인 경우
+            teamGender.add(malePlayer1);
+            teamGender.add(malePlayer2);
+        } else if (femalePlayer1 != null && femalePlayer2 != null) {
+            // 여자 두 명인 경우
+            teamGender.add(femalePlayer1);
+            teamGender.add(femalePlayer2);
+        } else {
+            // 예약되지 않은 플레이어가 있으면 "notReserved" 추가
+            if (player1 == null) {
+                teamGender.add("notReserved");
+            }
+            if (player2 == null) {
+                teamGender.add("notReserved");
+            }
+
+            // 남자나 여자가 하나만 있을 경우 추가
+            if (malePlayer1 != null) {
+                teamGender.add(malePlayer1);
+            }
+            if (femalePlayer1 != null) {
+                teamGender.add(femalePlayer1);
+            }
         }
 
         return teamGender;
