e01d55a87d989d5060aa2e39cc75c110f613ac2e
CSchoice <tkdlqjf@gmail.com>
2024-10-11 10:40:30 +0900

    hotfix : 레이팅 변화 로직 수정

--- src/main/java/ssafy/ddada/domain/match/service/MatchServiceImpl.java
+++ src/main/java/ssafy/ddada/domain/match/service/MatchServiceImpl.java
@@ -548,10 +548,6 @@ public class MatchServiceImpl implements MatchService {
                         false
                 );
             }
-            player.setRating(newRating);
-            player.setGameCount(player.getGameCount() + 1);
-
-
             // 레이팅 변화 기록
             RatingChange ratingChange = ratingChangeRepository
                     .findRatingChangeByMatchIdAndPlayerId(match.getId(), player.getId())
@@ -564,6 +560,10 @@ public class MatchServiceImpl implements MatchService {
                     );
             ratingChange.setRatingChange(newRating - player.getRating());
             ratingChangeRepository.save(ratingChange);
+
+            player.setRating(newRating);
+            player.setGameCount(player.getGameCount() + 1);
+
             playerRepository.save(player);
             rankingUtil.updatePlayerRating(player);
         }
